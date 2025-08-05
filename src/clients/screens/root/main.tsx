8898c9ca5f4880c4dbea62c8eb149a549c5c0225
Wahyu Agus Arifin <itpohgero@gmail.com>
2025-08-05 15:06:28 +0700

    Add button component and update styles; enhance global CSS and package dependencies

--- src/clients/screens/root/main.tsx
+++ src/clients/screens/root/main.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { useMain } from "@/clients/screens/root/hooks/useMain";
+import { Fragment } from "react";
+import { Button } from "@/clients/components/ui/button";
 
 export default function RootMainScreen() {
 	const { message } = useMain();
-	return <div>{message && <div>{message}</div>}</div>;
+	return (
+		<Fragment>
+			<Button>Button Components</Button>
+			<div>{message && <div>{message}</div>}</div>
+		</Fragment>
+	);
 }
